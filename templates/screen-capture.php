fac4d418b158bb58793d13f74fc98cce435e11a8
André Filipe <andre.r.flip@gmail.com>
2016-01-25 15:49:33 +0000

    Background color setted to white as default

--- templates/screen-capture.php
+++ templates/screen-capture.php
@@ -3,15 +3,20 @@ var page = require('webpage').create();
 page.viewportSize = {width: <?= $width ?>, height: <?= $height ?>};
 
 <?php if (isset($clipWidth) && $clipWidth && isset($clipHeight) && $clipHeight) : ?>
-    page.clipRect = {
+page.clipRect = {
     top: 0,
     left: 0,
     width: <?= $clipWidth ?>,
     height: <?= $clipHeight ?>
-    };
+};
 <?php endif ?>
 
 page.open('<?= $url ?>', function () {
+    /* This will set the page background color white */
+    page.evaluate(function() {
+        document.body.bgColor = '#FFFFFF';
+    });
+
     page.render('<?= $imageLocation ?>');
     phantom.exit();
 });
