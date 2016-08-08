0d8b069792880338be78021a3b62c7380467eb34
Limon Monte <limon.monte@gmail.com>
2016-08-08 17:05:47 +0200

    Add ability to set custom path to PhantomJS binary

--- src/Capture.php
+++ src/Capture.php
@@ -178,6 +178,20 @@ class Capture
     }
 
     /**
+     * Sets the path to PhantomJS binary, example: "/usr/bin"
+     *
+     * @param string $path
+     */
+    public function setBinPath($binPath)
+    {
+        $binPath = rtrim($binPath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        if (!file_exists($binPath . 'phantomjs')) {
+            throw new \Exception("Bin directory should contain the phantomjs file!");
+        }
+        $this->binPath = $binPath;
+    }
+
+    /**
      * Sets the url to screenshot
      *
      * @param string $url URL
